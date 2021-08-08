c083592cc7f8622bce157cea004ca8b0dd8d9334
mmoore483 <mary.e.moore@hotmail.co.uk>
2021-08-08 16:00:36 +0000

    Add random question generator in js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,40 @@ The following function importData retrieves a csv and splits it into rows by lin
 into columns by comma
 */
 
+
 async function importData() {
-    const response = await fetch("assets/csv/quiz.csv");
-    const data = await response.text();
-    let questionArray = [];
-    let answerArray = [];
-    const rows = data.split(/\r?\n|\r/).slice(1);
-    for (row of rows){
-      const columns = row.split(',');
+  let questionArray = [];
+  let answerArray = [];
+  const response = await fetch("assets/csv/quiz.csv");
+  const data = await response.text();
+
+  const rows = data.split(/\r?\n|\r/).slice(1);
+  for (row of rows){
+    const columns = row.split(',');
      
-      //Loop to create a question array
-      for (let i = 0; i < columns.length; i+=2){
-       questionArray.push(columns[i]);
-      }
-      //Loop to create an answer array
-      for (let i = 1; i < columns.length; i+=2){
-        answerArray.push(columns[i]);
-       }   
+    //Loop to create a question array
+    for (let i = 0; i < columns.length; i+=2){
+      questionArray.push(columns[i]);
+    }
+    //Loop to create an answer array
+    for (let i = 1; i < columns.length; i+=2){
+      answerArray.push(columns[i]);
+    }   
 }
-console.log(questionArray);
-console.log(answerArray);
+
+//Generate the random question
+let num = randomNumber();
+document.getElementById("question").textContent = questionArray[num];
 }
+
+
 importData();
+
+
+//Random number generator for question selection
+function randomNumber() {
+  let num1 = Math.floor(Math.random()* 20);
+  return num1;
+}
+
+
